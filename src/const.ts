198af236f73a61e7a2da374fcd121fe1a909bfd8
SHUBHAM KASHYAP <info@aladinntech.in>
2023-06-20 16:19:26 +0530

    final

--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,12 @@
 export const constants = {
   jwtConstants: 'my_secret',
   x_api_key: 'x-api-key',
-  db: 'mongodb+srv://air-bnb:air-bnb@cluster0.menpm.mongodb.net/air-bnb',
+  db: '',
 };
 
 export const AWScredentials = {
-  accessKeyId: 'AKIAQDEENMG732VTIBP2',
-  secretAccessKey: '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',
-  region: 'eu-west-2',
-  bucket: 'quikbook',
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: '',
+  bucket: '',
 };
